9f3c7c2848dcfe5560829e276c5f6bc9081826c2
Alena Nikitina <nikitina.alena5@yandex.ru>
2026-02-27 20:55:42 +0300

    Add email.py

--- test_homework/email.py
+++ test_homework/email.py
@@ -11,7 +11,7 @@ email5 = {
         "Please reply soon.\n"
         "Regards,\n"
         "Team"
-    )
+    ),
 }
 
 # 2. Добавьте дату отправки
@@ -23,38 +23,42 @@ email5["from"] = email5["from"].strip().lower()
 email5["to"] = email5["to"].strip().lower()
 
 # 4. Извлеките логин и домен отправителя
-login = email5["from"].split("@")[0]
-domain = email5["from"].split("@")[1]
+login, domain = email5["from"].split("@")
 
 # 5. Создайте сокращённую версию текста
 email5["short_body"] = email5["body"][:10] + "..."
 
 # 6. Списки доменов
-personal_domains = {
-    'gmail.com',
-    'list.ru',
-    'yahoo.com',
-    'outlook.com',
-    'hotmail.com',
-    'icloud.com',
-    'yandex.ru',
-    'mail.ru',
-    'list.ru',
-    'bk.ru',
-    'inbox.ru'
-}
+personal_domains = list(
+    {
+        "gmail.com",
+        "list.ru",
+        "yahoo.com",
+        "outlook.com",
+        "hotmail.com",
+        "icloud.com",
+        "yandex.ru",
+        "mail.ru",
+        "list.ru",
+        "bk.ru",
+        "inbox.ru",
+    }
+)
 
-corporate_domains = {
-    'company.ru',
-    'corporation.com',
-    'university.edu',
-    'organization.org',
-    'company.ru',
-    'business.net'
-}
+corporate_domains = list(
+    {
+        "company.ru",
+        "corporation.com",
+        "university.edu",
+        "organization.org",
+        "company.ru",
+        "business.net",
+    }
+)
 
 # 7. Проверьте что в списке личных и корпоративных доменов нет пересечений
-no_common_domains = personal_domains.isdisjoint(corporate_domains)
+no_common_domains = set(personal_domains).isdisjoint(set(corporate_domains))
+assert no_common_domains
 
 # 8. Проверьте «корпоративность» отправителя
 is_corporate = domain in corporate_domains
@@ -63,18 +67,16 @@ is_corporate = domain in corporate_domains
 email5["clean_body"] = email5["body"].replace("\t", " ").replace("\n", " ")
 
 # 10. Сформируйте текст отправленного письма
-email5["sent_text"] = f"""
-Кому: {email5["to"]}, от {email5["from"]}
+email5["sent_text"] = f"""Кому: {email5["to"]}, от {email5["from"]}
 Тема: {email5["subject"]}, дата {email5["date"]}
-{email5["clean_body"]}
-"""
+{email5["clean_body"]}"""
 
 # 11. Рассчитайте количество страниц печати
 pages = (len(email5["sent_text"]) + 499) // 500
 
 # 12. Проверьте пустоту темы и тела письма
-is_subject_empty = not email5["subject"]
-is_body_empty = not email5["body"]
+is_subject_empty = not email5["subject"].strip()
+is_body_empty = not email5["body"].strip()
 
 # 13. Создайте «маску» e-mail отправителя
 email5["masked_from"] = login[:2] + "***@" + domain
